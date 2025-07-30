6caccf7ae5f973d7c29ccd8f58687f93b35477e4
Leonid Lachmann <leocyanid@gmail.com>
2025-07-30 16:58:29 -0400

    Terraform_task_3.9

--- main.tf
+++ main.tf
@@ -1,27 +1,14 @@
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.existing_rg.location
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.existing_rg.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_public_ip" "main" {
-  name                = "${var.prefix}-public-ip"
-  location            = data.azurerm_resource_group.existing_rg.location
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-
-  lifecycle {
-    create_before_destroy = true
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_network_security_group" "main" {
@@ -54,76 +41,19 @@ resource "azurerm_network_security_group" "main" {
   }
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = data.azurerm_resource_group.existing_rg.location
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.main.id
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "azurerm_network_interface_security_group_association" "main" {
-  network_interface_id      = azurerm_network_interface.main.id
+  network_interface_id      = data.azurerm_network_interface.existing_nic.id
   network_security_group_id = azurerm_network_security_group.main.id
-}
-
-resource "azurerm_virtual_machine" "main" {
-  name                            = "${var.prefix}-vm"
-  location                        = data.azurerm_resource_group.existing_rg.location
-  resource_group_name             = data.azurerm_resource_group.existing_rg.name
-  vm_size                         = "Standard_B1s"
-  network_interface_ids           = [azurerm_network_interface.main.id]
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-    disk_size_gb      = 30
-  }
-
-  os_profile {
-    computer_name  = "${var.prefix}-vm"
-    admin_username = "leoleiden"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      path     = "/home/leoleiden/.ssh/authorized_keys"
-      key_data = file("${path.module}/Terra3.pem.pub")
-    }
-  }
-
-  tags = {
-    environment = "staging"
-  }
 }
 
 resource "null_resource" "vm_provisioning" {
   triggers = {
-    vm_id              = azurerm_virtual_machine.main.id
+    vm_id              = data.azurerm_virtual_machine.existing_vm.id
     index_html_content = filemd5("${path.module}/index.html")
   }
 
   depends_on = [
-    azurerm_virtual_machine.main,
+    data.azurerm_virtual_machine.existing_vm,
     azurerm_network_interface_security_group_association.main
   ]
 
@@ -131,7 +61,7 @@ resource "null_resource" "vm_provisioning" {
     type        = "ssh"
     user        = "leoleiden"
     private_key = file("${path.module}/Terra3.pem")
-    host        = azurerm_public_ip.main.ip_address
+    host        = data.azurerm_public_ip.existing_public_ip.ip_address
     timeout     = "10m"
   }
 
